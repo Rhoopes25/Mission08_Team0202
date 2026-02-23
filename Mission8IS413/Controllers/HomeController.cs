0ed4b8471c604ed5110425c83a0d1941abe5912c
Rhoopes25 <rachhoopes1@gmail.com>
2026-02-23 09:31:11 -0700

    renamed index to quadrants

--- Mission8IS413/Controllers/HomeController.cs
+++ Mission8IS413/Controllers/HomeController.cs
@@ -6,19 +6,10 @@ namespace Mission8IS413.Controllers;
 
 public class HomeController : Controller
 {
-    public IActionResult Index()
+    public IActionResult Quadrants()
     {
         return View();
     }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+    
 }
