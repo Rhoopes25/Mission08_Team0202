7407d6b0401234836027f1e9ae5f360ef09d590e
jkethington <jkethington@gmail.com>
2026-02-23 10:31:15 -0700

    Category and Task

---
+++ Mission8IS413/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mission8IS413.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public string CategoryName { get; set; } = "";
+    }
+}
--- Mission8IS413/Models/Task.cs
+++ Mission8IS413/Models/Task.cs
@@ -1,19 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Mission8IS413.Models
 {
     public class Task
     {
+        [Key]
         public int TaskId { get; set; }
 
         [Required(ErrorMessage = "Please enter a task name.")]
         public string TaskName { get; set; }
         public DateOnly TaskDue { get; set; }
 
+        [Required(ErrorMessage = "Please enter a task Quadrant.")]
         public int TaskQuadrant { get; set; }
 
-        [Required(ErrorMessage = "Please enter a task category.")]
-        public string TaskCategory { get; set; }
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category? TaskCategory { get; set; }
         public bool TaskCompleted { get; set; }
     }
 }
